56d98fd860588edf579da84ef894553ac9ea4a53
Showkath <iamshowkath@gmail.com>
2023-07-12 16:17:03 +0530

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,37 @@
+package com.sap.backend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import com.sap.backend.config.BProperties;
+
+
+
+@SpringBootApplication(scanBasePackages = {"com.sap.backend" ,"org.springframework.cloud"})
+@EnableAsync
+
+public class BApplication {
+	
+	@Autowired
+	BProperties props;
+
+	public static void main(String[] args) {
+		SpringApplication.run(BApplication.class, args);
+	}
+
+	@Bean("threadPoolTaskExecutor")
+	public TaskExecutor getAsyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(props.getCorePoolSize());
+		executor.setMaxPoolSize(props.getMaxPoolSize());
+		executor.setWaitForTasksToCompleteOnShutdown(true);
+		executor.setThreadNamePrefix("Async-");
+		return executor;
+	}
+}
+
